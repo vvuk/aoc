1933ba5a74e3088e1658e79370ca4eeaf8cf01fb
Vladimir Vukicevic <vladimir@pobox.com>
2022-11-22 14:07:05 +0000

    Pending changes exported from your codespace

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
